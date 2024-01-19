31a3f6037cb3ce7d4a7ac7b723943932f67f7d4b
Rsirp0c <122833217+Rsirp0c@users.noreply.github.com>
2024-01-19 13:07:40 -0500

    Update 3_🌏_Contacts.py

--- pages/3_🌏_Contacts.py
+++ pages/3_🌏_Contacts.py
@@ -1,24 +1,22 @@
 import streamlit as st 
-import streamlit.components.v1 as components
 from constant import *
 
 st.set_page_config(page_title="Contacts", page_icon="🌏") #layout="wide"
 
-st.markdown("##### 📞 Phone: +1 971-900-6780")   
-# st.markdown("#### 🇨🇳 Nationality: Chinese")
-st.markdown("##### ✉️ Email: haorancheng@brandeis.edu")
+st.markdown(f"##### 📞 Phone: +1 {phone}")   
+st.markdown(f"##### ✉️ Email: {email}")
 with st.container():
     col1, col2 = st.columns([0.1, 3])
     with col1:
         st.write(linkedin_logo, unsafe_allow_html=True)
     with col2:
-        st.markdown("#####  Linkedin: www.linkedin.com/in/haoran-cheng-018b9318b/")
+        st.markdown(f"#####  Linkedin: {linkedin_link}")
 with st.container():
     col1, col2 = st.columns([0.1, 3])
     with col1:
         st.write(github_logo, unsafe_allow_html=True)
     with col2:
-        st.markdown("#####  Github: https://github.com/Rsirp0c?tab=repositories")
+        st.markdown(f"#####  Github: {github_link}")
 
 
 
